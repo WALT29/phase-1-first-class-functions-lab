0b3aea658703901375a0bea7ebb93b8c5296528f
WALTER DAVID IRUNGU <daveirungu20@gmail.com>
2024-06-29 04:09:54 +0300

    functions lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers=([x,y,z,a])=>{
+    return [x,y];
+}
+
+console.log(returnFirstTwoDrivers(drivers));
+
+const returnLastTwoDrivers=([x,y,z,a])=>{
+    return[z,a];
+}
+console.log(returnLastTwoDrivers(drivers));
+
+let selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
+
+const createFareMultiplier=(fare)=>{
+
+    return function(multiplier){
+        return fare*multiplier;
+    }
+}
+
+const fareDoubler=createFareMultiplier(2);
+console.log(fareDoubler(4));
+
+const fareTripler=createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers,selectionFunction){
+    return selectionFunction(drivers);
+
+}
